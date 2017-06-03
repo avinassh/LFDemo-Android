b1876ab1c1d954f31ff82c20771f3543d9a8dd6f
avi <hi@avi.im>
2017-06-03 22:04:44 +0530

    Add litlefinger

--- app/src/main/java/im/avi/lfdemo/MainActivity.java
+++ app/src/main/java/im/avi/lfdemo/MainActivity.java
@@ -3,11 +3,15 @@ package im.avi.lfdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import im.avi.littlefinger.LittleFinger;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LittleFinger.start(this, "https://your-heroku-app.herokuapp.com/status");
     }
 }
